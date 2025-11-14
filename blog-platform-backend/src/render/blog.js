32b6d55908b52928373fbe4beec46a1109398ba1
quoctuan2005 <nqtuan05@gmail.com>
2025-11-14 21:50:10 +0700

    up

--- blog-platform-backend/src/render/blog.js
+++ blog-platform-backend/src/render/blog.js
@@ -710,11 +710,12 @@ async function renderPublicBlog(req, res) {
               var storageKey = 'bloggo_visitorId';
               var visitorId = localStorage.getItem(storageKey);
               if (!visitorId) {
-                visitorId = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-                  (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-                );
-                localStorage.setItem(storageKey, visitorId);
-              }
+              visitorId = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+              });
+              localStorage.setItem(storageKey, visitorId);
+            }
               var startTime = performance.now();
               var maxScroll = 0;
               window.addEventListener('scroll', function(){
